4945d6d4c520ff60642d9baa4b8d282e938c8b89
GjinPrelvukaj <132224503+GjinPrelvukaj@users.noreply.github.com>
2023-08-09 23:12:56 +0200

    Initial Commit

---
+++ app/(auth)/onboarding/page.tsx
@@ -0,0 +1,11 @@
+async function Page() {
+    return(
+        <main>
+            <h1 className="head-text">
+                <h1>On Boarding</h1>
+            </h1>
+        </main>
+    )
+}
+
+export default Page;
